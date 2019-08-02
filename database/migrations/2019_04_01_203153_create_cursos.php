951649abb39c2a2c9a7b159892016a55220dd0d3
rafaell_junioor <juniorrafael95@yahoo.com.br>
2019-08-02 14:44:21 -0300

    alteração de timestamps

--- database/migrations/2019_04_01_203153_create_cursos.php
+++ database/migrations/2019_04_01_203153_create_cursos.php
@@ -16,8 +16,9 @@ class CreateCursos extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome');
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent();
+            //$table->timestamp('created_at')->useCurrent();
+            //$table->timestamp('updated_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
